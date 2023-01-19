1157aef2b14dc498976e2cbc2d317c073b86c03d
parthiv chovatiya <parthivchovatiya0408@gmail.com>
2023-01-19 16:51:36 +0530

    main

--- model1/lib/app_data/student_data.dart
+++ model1/lib/app_data/student_data.dart
@@ -1,20 +1,36 @@
 class StudentData {
   static Map<String, dynamic> studentInformation = {
-    "stu1": {
-      'name': 'janki',
-      'rollno': 20,
-      'course': "flutter",
-    },
-    "stu2": {
-      'name': 'parthiv',
+    'stu1': {
+      'name': 'Akash',
       'rollno': 10,
+      'course': "interior design",
+      'fees': 120000,
+    },
+    'stu2': {
+      'name': 'parthiv',
+      'rollno': 20,
       'course': "flutter",
       'fees': 55000,
     },
-    "stu3": {
-      'name': 'kajal',
-      'rollno': 25,
-      'course': "nodejs",
+    'stu3': {
+      'name': 'kishan',
+      'rollno': 30,
+      'course': "Java",
+    },
+    'stu4': {
+      'name': 'krunal',
+      'rollno': 40,
+      'course': "interior design",
+    },
+    'stu5': {
+      'name': 'keyur',
+      'rollno': 50,
+      'course': "Flutter",
+    },
+    'stu6': {
+      'name': 'Meet',
+      'rollno': 60,
+      'course': "Flutter",
     },
   };
 }
--- model1/lib/model/student_model.dart
+++ model1/lib/model/student_model.dart
@@ -1,10 +1,27 @@
 class StudentModel {
   final Student1? student1;
+  final Student2? student2;
+  final Student3? student3;
+  final Student4? student4;
+  final Student5? student5;
+  final Student6? student6;
 
-  StudentModel({this.student1});
+  StudentModel({
+    this.student1,
+    this.student2,
+    this.student3,
+    this.student4,
+    this.student5,
+    this.student6,
+  });
   factory StudentModel.fromJson(Map<String, dynamic> json) {
     return StudentModel(
       student1: Student1.fromJson(json['stu1']),
+      student2: Student2.fromJson(json['stu2']),
+      student3: Student3.fromJson(json['stu3']),
+      student4: Student4.fromJson(json['stu4']),
+      student5: Student5.fromJson(json['stu5']),
+      student6: Student6.fromJson(json['stu6']),
     );
   }
 }
@@ -13,14 +30,115 @@ class Student1 {
   final String? name;
   final int? rollnum;
   final String? course;
+  final int? fees;
 
   Student1({
+    this.fees,
     this.name,
     this.rollnum,
     this.course,
   });
   factory Student1.fromJson(Map<String, dynamic> json) {
     return Student1(
+      name: json['name'],
+      rollnum: json['rollno'],
+      course: json['course'],
+      fees: json['fees'],
+    );
+  }
+}
+
+class Student2 {
+  final String? name;
+  final int? rollnum;
+  final String? course;
+  final int? fees;
+
+  Student2({
+    this.name,
+    this.rollnum,
+    this.course,
+    this.fees,
+  });
+  factory Student2.fromJson(Map<String, dynamic> json) {
+    return Student2(
+      name: json['name'],
+      rollnum: json['rollno'],
+      course: json['course'],
+      fees: json['fees'],
+    );
+  }
+}
+
+class Student3 {
+  final String? name;
+  final int? rollnum;
+  final String? course;
+
+  Student3({
+    this.name,
+    this.rollnum,
+    this.course,
+  });
+  factory Student3.fromJson(Map<String, dynamic> json) {
+    return Student3(
+      name: json['name'],
+      rollnum: json['rollno'],
+      course: json['course'],
+    );
+  }
+}
+
+class Student4 {
+  final String? name;
+  final int? rollnum;
+  final String? course;
+
+  Student4({
+    this.name,
+    this.rollnum,
+    this.course,
+  });
+  factory Student4.fromJson(Map<String, dynamic> json) {
+    return Student4(
+      name: json['name'],
+      rollnum: json['rollno'],
+      course: json['course'],
+    );
+  }
+}
+
+class Student5 {
+  final String? name;
+  final int? rollnum;
+  final String? course;
+
+  Student5({
+    this.name,
+    this.rollnum,
+    this.course,
+  });
+  factory Student5.fromJson(Map<String, dynamic> json) {
+    return Student5(
+      name: json['name'],
+      rollnum: json['rollno'],
+      course: json['course'],
+    );
+  }
+}
+
+class Student6 {
+  final String? name;
+  final int? rollnum;
+  final String? course;
+
+  Student6({
+    this.name,
+    this.rollnum,
+    this.course,
+  });
+  factory Student6.fromJson(Map<String, dynamic> json) {
+    return Student6(
       name: json['name'],
       rollnum: json['rollno'],
       course: json['course'],
